192839ab19c9d230daa7ded2543a31f668c55823
Darkfire16 <gmodman12@gmail.com>
2016-05-02 16:51:25 -0400

    Added intepreter start.

---
+++ grove.py
@@ -0,0 +1,15 @@
+'''Project 5
+5-2-16
+OOPLS
+Zared Hollabaugh
+Caelan Mayberry
+Stuart Bowman
+'''
+
+exec(open("grove_parse.py").read())
+
+if __name__ == "__main__":
+    while True:
+        choice = input("Grove>>")
+        root = parse(choice)
+        #print(root)
