4ad0c24d77904df96410d4f23d1ba6f7ad0dfa0e
Darkfire16 <gmodman12@gmail.com>
2016-05-03 12:56:55 -0400

    Bug fixes.

--- grove_parse.py
+++ grove_parse.py
@@ -64,6 +64,7 @@ def parse_tokens(tokens):
 
     elif start == "set":
         (varname, tokens) = parse_tokens(tokens[1:])
+        print(tokens)
         expect(tokens[0], "=")
         (child, tokens) = parse_tokens(tokens[1:])
         return (Stmt(varname, child), tokens)
